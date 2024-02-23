b207538117ac6bcfba1f846ee603c414a64b1a45
Matt Applegate <45499879+mbapplegate@users.noreply.github.com>
2024-02-23 15:38:55 -0500

    updated laser with vector asset

--- scenes/LaserSource/laser_source.gd
+++ scenes/LaserSource/laser_source.gd
@@ -3,19 +3,18 @@ extends pushableObject
 @export var halfAngle = 0.0
 @export var numRaysPerTimeout : int = 1
 @export var rayColor : Vector3 = Vector3(1.0, 0, 1.0)
-@onready var barrelShape = $Sprite2D/barrelSprite/barrelArea/barrelShape
+@onready var barrelShape = $Stage/laserBarrel/barrelArea/barrelShape
 @onready var ray = preload("res://scenes/LightPacket/light_packet.tscn")
 
 var rng = RandomNumberGenerator.new()
 var laserParent= null
-const halfHeight = 12.0
+const halfHeight = 32.0
 
 func _ready():
 	#halfHeight = BARREL_WIDTH * self.scale.x
 	#print(halfHeight)
 	#isEnergized = true
 	laserParent = get_parent()
-	print(laserParent)
 	pass
 	
 func _ray_hit(photonObj:Object, _collPoint:Vector2, _collNormal:Vector2, _collider:Object):
@@ -37,7 +36,7 @@ func _on_timer_timeout():
 			instance = ray.instantiate()
 			instance.propDir = Vector2(cos(angle+sprite.rotation),sin(angle+sprite.rotation))
 			instance.position = to_global(Vector2(barrelPosition.x+yLoc*sin(sprite.rotation), -barrelPosition.y-yLoc*cos(sprite.rotation)))
-			print(barrelPosition)
+			
 			instance.rayColor = rayColor
 			instance.scale[0] = 1.0/self.scale[0]
 			instance.scale[1] = 1.0 / self.scale[1]
