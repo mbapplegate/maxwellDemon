82a24117ffcb655eccc6e713637cd86db6ca407a
Matt Applegate <matthewbapplegate@gmail.com>
2024-03-07 11:39:01 -0500

    Set up link between detector and screen

--- scenes/DetectorElement/detector_element.gd
+++ scenes/DetectorElement/detector_element.gd
@@ -1,9 +1,12 @@
 extends Node2D
+@export var absorbs:bool = false
 @export var detectorHeight:int = 128
-@export var detectorWidth:int = 4
+@export var detectorWidth:int = 16
 
 @onready var detPoly = $detectorPoly
 @onready var detShape = $detectorPoly/detectorArea/detectorShape
+
+signal detectedPhoton(loc:float, en:float)
 
 func _ready():
 	detPoly.polygon = PackedVector2Array([Vector2(-detectorWidth/2.0,-detectorHeight/2.0),
@@ -11,5 +14,12 @@ func _ready():
 	Vector2(detectorWidth/2.0,detectorHeight/2.0),
 	Vector2(detectorWidth/2.0,-detectorHeight/2.0)])
 	
-	detShape.shape.size = Vector2(detectorWidth,detectorHeight)
+	detShape.shape.size = Vector2(2,detectorHeight)
 	detShape.position = Vector2.ZERO
+	
+func _ray_hit(photonObj:Object, collPoint:Vector2, _collNormal:Vector2, _collider:Object):
+	var localPoint = to_local(collPoint)
+	var pointPct = localPoint.y / detectorHeight + 0.5
+	detectedPhoton.emit(pointPct,photonObj.energy)
+	if (absorbs):
+		photonObj.rayDying = true
--- scenes/DetectorScreen/detector_screen.gd
+++ scenes/DetectorScreen/detector_screen.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
-const NUM_GRAPH_POINTS = 100
+@export var multiplier = 1.0
+@export var multiplierFixed = false
+const NUM_GRAPH_POINTS = 16
 const SCREEN_HEIGHT = 118.0
 const SCREEN_WIDTH = 64.0
-const ZERO_LOC = -24
+const ZERO_LOC = -30
+const MULTIPLIER_MAX = 1024.0
+const MULTIPLIER_MIN = 1/32.0
 @onready var graphLine = $screenSprite/graphLine
 var rng = RandomNumberGenerator.new()
-var multiplier = 1
 
 var graphXVals = []
 var graphYVals = []
+var increaseMultiplier = false
+var decreaseMultiplier = false
 
 func _ready():
+	for child in get_children():
+		if child.name == "DetectorElement":
+			child.detectedPhoton.connect(_packet_detected)
+			
 	graphLine.clear_points()
 	graphXVals.resize(NUM_GRAPH_POINTS)
 	graphXVals.fill(0.0)
@@ -20,36 +29,60 @@ func _ready():
 
 	for i in NUM_GRAPH_POINTS:
 		graphYVals[i] = (-SCREEN_HEIGHT/2.0)+(SCREEN_HEIGHT/(NUM_GRAPH_POINTS-1))*i
-		graphXVals[i] = rng.randfn(0,5)+ZERO_LOC
-		graphLine.add_point(Vector2(graphXVals[i],graphYVals[i]))
+		graphLine.add_point(Vector2(ZERO_LOC,graphYVals[i]))
 
 func _draw_graph():
+	var numMax = 0
+	var numMin = 0
+	var maxX = -1000
+	var minX = 1000
+	var sumX = 0
 	for i in NUM_GRAPH_POINTS:
-		graphLine.set_point_position(i,Vector2(graphXVals[i]/multiplier,graphYVals[i]))
+		var thisXVal = min(SCREEN_WIDTH/2.0,max(-SCREEN_WIDTH/2.0,(graphXVals[i]*multiplier+ZERO_LOC)))
+		sumX += thisXVal
+		if thisXVal > maxX:
+			maxX = thisXVal
+		if thisXVal < minX:
+			minX = thisXVal
+			
+		if thisXVal == SCREEN_WIDTH/2.0:
+			numMax += 1
+		elif thisXVal == -SCREEN_WIDTH/2.0:
+			numMin += 1
+			
+		graphLine.set_point_position(i,Vector2(thisXVal,graphYVals[i]))
+	
+	if not multiplierFixed:
+		var avgX = sumX / NUM_GRAPH_POINTS
+		if avgX > SCREEN_WIDTH/4.0 or maxX == SCREEN_WIDTH/2.0:
+			decreaseMultiplier = true
+		if maxX < 0:
+			increaseMultiplier = true
+		
+		print("Avg: ", avgX, ", Max: ", maxX, ", Inc: ",increaseMultiplier, ", Dec: ", decreaseMultiplier, ", Mult: ",multiplier)
 		
 func _add_noise(noiseScale:float):
 	for i in NUM_GRAPH_POINTS:
-		graphXVals[i] = max(-SCREEN_WIDTH/2.0,min(SCREEN_WIDTH/2.0,graphXVals[i] + rng.randfn(0,noiseScale)+ZERO_LOC))
+		graphXVals[i] += rng.randfn(0,noiseScale)
 		
 func _clear_graph():
 	graphXVals.fill(0.0)
 
 func _packet_detected(location:float, energy:float):
-	var ind = int(round(location*NUM_GRAPH_POINTS))
-	graphXVals[ind] += energy
+	var ind = max(0,min((NUM_GRAPH_POINTS-1),int(round(location*NUM_GRAPH_POINTS))))
+	graphXVals[ind] += (energy)
 
-func _adjust_multiplier():
-	var numMax = graphXVals.count(SCREEN_WIDTH/2.0)
-	if numMax > (0.6*NUM_GRAPH_POINTS):
-		multiplier /= 2.0
-	else:
-		var numMin = graphXVals.count(-SCREEN_WIDTH/2.0)
-		if numMin > (0.6*NUM_GRAPH_POINTS):
-			multiplier *= 2
-		
 			
 func _on_timer_timeout():
-	_clear_graph()
-	_add_noise(25)
-	_adjust_multiplier()
+	
+	if increaseMultiplier and multiplier < MULTIPLIER_MAX:
+		multiplier *= 2
+		increaseMultiplier = false
+		
+	elif decreaseMultiplier and multiplier > MULTIPLIER_MIN:
+		multiplier /= 2
+		decreaseMultiplier = false
+	
+	_add_noise(1)
 	_draw_graph()
+	_clear_graph()
