0a525e9c194ce0321cafaf7f3016ed55fd69bb03
Matt Applegate <matthewbapplegate@gmail.com>
2024-04-10 17:02:56 -0400

    added options to main menu

--- utilities/mainMenu/main_menu.gd
+++ utilities/mainMenu/main_menu.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+func _ready():
+	$VBoxContainer/startGame.grab_focus()
+	
 func _on_button_pressed():
 	SceneSwitcher.ChangeScene("Level001")
+
+func _on_select_level_pressed():
+	SceneSwitcher.ChangeScene("MainMenu")
+	
+func _on_quit_game_pressed():
+	SceneSwitcher.QuitGame() # Replace with function body.
