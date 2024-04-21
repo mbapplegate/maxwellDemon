87a84c00204b2e539f8f033dcc51af80128d9456
Matt Applegate <matthewbapplegate@gmail.com>
2024-04-21 11:49:12 -0400

    Backs of mirrors no longer reflect

--- scenes/FlatMirror/flat_mirror.gd
+++ scenes/FlatMirror/flat_mirror.gd
@@ -7,11 +7,14 @@ func _ready():
 	if not isRotatable and initialAngle != 0:
 		mirror.rotation=deg_to_rad(initialAngle)
 
-func _ray_hit(photonObj:Object, collPoint:Vector2, collNormal:Vector2, _collider:Object):
-	var ref = randf()
-	if (ref < reflectivity):
-		var newDir = photonObj.propDir.bounce(collNormal).normalized()
-		photonObj.propDir = newDir
-		photonObj.ray_add_point(photonObj.to_local(collPoint))
+func _ray_hit(photonObj:Object, collPoint:Vector2, collNormal:Vector2, collider:Object):
+	if collider.name == "Front":
+		var ref = randf()
+		if (ref < reflectivity):
+			var newDir = photonObj.propDir.bounce(collNormal).normalized()
+			photonObj.propDir = newDir
+			photonObj.ray_add_point(photonObj.to_local(collPoint))
+		else:
+			photonObj.rayDying = true
 	else:
 		photonObj.rayDying = true
