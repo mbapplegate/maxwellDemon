2b66de4785c2631bd44ad78018db204980dcd07b
Matt Applegate <matthewbapplegate@gmail.com>
2024-04-21 11:41:15 -0400

    Fixed initial rotation issue

--- scenes/BeamSplitter/beam_splitter.gd
+++ scenes/BeamSplitter/beam_splitter.gd
@@ -4,7 +4,8 @@ extends pushableObject
 @export var splitterIndex = 2.0     #Index of refraction of the splitter
 
 @onready var ray = preload("res://scenes/LightPacket/light_packet.tscn")
-@onready var splitter = $Stage/splitterArea
+@onready var splitter = $Stage/Sprite2D/splitterArea
+@onready var splitterSprite = $Stage/Sprite2D
 
 const mediumIndex = 1.0     #Index of the medium surrounding the beam splitter
 var splitterParent = null   #Parent object of the beamsplitter
@@ -12,6 +13,8 @@ var splitterParent = null   #Parent object of the beamsplitter
 func _ready():
 	#Not energizeable
 	isEnergizeable = false
+	if not isRotatable and initialAngle != 0:
+		splitterSprite.rotation=deg_to_rad(initialAngle)
 	splitterParent = get_parent()
 	
 #Function to handle if a ray collides with either the walls of the cube or the splitter
