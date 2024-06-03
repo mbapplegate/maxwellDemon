803d83928391d186dc71b65dd817808c6ff59654
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-02 20:30:26 -0400

    Simplified mirror lesson

--- lessons/mirror/mirror_lesson.gd
+++ lessons/mirror/mirror_lesson.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @export var nextSceneAlias = "Level007"
 const THIS_SCENE_ALIAS = "MirrorLesson"
-
+const ANGLE_DEMO_LEN = 75
 var signalEmitted : bool = false
-var dirIdx = 0
+var rotatingCCW = true
 @onready var lref = $LeftReflected
-@onready var rref = $RightReflected
+#@onready var rref = $RightReflected
 
 signal nextScene(sceneAlias)
 	
@@ -14,7 +14,12 @@ func _ready():
 	for child in get_children():
 		if child is pushableObject:
 			child.initialize()
-	#animateLeft()
+	var rot = $FlatMirror.getRotation()
+	var newLoc = changeLineAngle(rot)
+	
+	$LeftIncident.set_point_position(2,newLoc)
+	$LeftReflected.set_point_position(2,newLoc)
+	$Normal.set_point_position(1,Vector2(85*sin(rot),-85*cos(rot)))
 
 func _input(event):
 	if not signalEmitted and event.is_pressed():
@@ -25,38 +30,66 @@ func animateLeftForward():
 	var tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(lref,"rotation",PI-deg_to_rad(90-$FlatMirror.initialAngle),3)
+	tween.tween_property(lref,"rotation",$FlatMirror.getRotation()+PI/2.0,0.75)
 	
-func animateRightForward():
-	var tween = get_tree().create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(rref,"rotation",PI-deg_to_rad(90-$FlatMirror2.initialAngle),3)
+#func animateRightForward():
+	#var tween = get_tree().create_tween()
+	#tween.set_ease(Tween.EASE_IN_OUT)
+	#tween.set_trans(Tween.TRANS_CUBIC)
+	#tween.tween_property(rref,"rotation",PI-deg_to_rad(90-$FlatMirror2.initialAngle),3)
 	
 func animateLeftBack():
 	var tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(lref,"rotation",0,3)
+	tween.tween_property(lref,"rotation",0,0.75)
 	
-func animateRightBack():
-	var tween = get_tree().create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(rref,"rotation",0,3)
+#func animateRightBack():
+	#var tween = get_tree().create_tween()
+	#tween.set_ease(Tween.EASE_IN_OUT)
+	#tween.set_trans(Tween.TRANS_CUBIC)
+	#tween.tween_property(rref,"rotation",0,3)
 
+func changeLineAngle(angleInRad:float)->Vector2:
+	var correctedAngle = PI/2.0 - angleInRad
+	var xLoc = -sin(correctedAngle) * ANGLE_DEMO_LEN
+	var yLoc = -cos(correctedAngle) * ANGLE_DEMO_LEN
+	return Vector2(xLoc,yLoc)
 
 func _on_timer_timeout():
 	if lref.rotation == 0:
 		await animateLeftForward()
+		$Timer.start()
 	else:
 		await animateLeftBack()
-	$Timer2.start()
+		$Timer2.start()
 
 
 func _on_timer_2_timeout():
-	if rref.rotation == 0:
-		await animateRightForward()
+	#if rref.rotation == 0:
+		#await animateRightForward()
+	#else:
+		#await animateRightBack()
+	if rotatingCCW:
+		$FlatMirror.rotateCCW()
 	else:
-		await animateRightBack()
+		$FlatMirror.rotateCW()
+	print("Angle: ",rad_to_deg($FlatMirror.getRotation()))
+	if rad_to_deg($FlatMirror.getRotation()) <= 10:
+		rotatingCCW = false
+	if $FlatMirror.getRotation() >= deg_to_rad($FlatMirror.initialAngle-2):
+		rotatingCCW = true
+	
+	var newLoc = changeLineAngle($FlatMirror.getRotation())
+	#print($LeftIncident.points[2])
+	#var tween = get_tree().create_tween()
+	#tween.set_ease(Tween.EASE_IN_OUT)
+	#tween.set_trans(Tween.TRANS_CUBIC)
+	#tween.tween_property($LeftIncident,"points[2]",newLoc,0.5)
+	var rot = $FlatMirror.getRotation()
+	$Normal.set_point_position(1,Vector2(85*sin(rot),-85*cos(rot)))
+	$LeftIncident.set_point_position(2,newLoc)
+	$LeftReflected.set_point_position(2,newLoc)
+	#$LeftIncident.position.y-=2
+	#$LeftReflected.position.y-=2
 	$Timer.start()
