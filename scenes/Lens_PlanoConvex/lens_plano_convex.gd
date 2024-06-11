b535f211df70f4a60aab6ba4cb1b9fe61884bebc
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-11 16:54:24 -0400

    Sub-pixel lens refraction

--- scenes/Lens_PlanoConvex/lens_plano_convex.gd
+++ scenes/Lens_PlanoConvex/lens_plano_convex.gd
@@ -1,6 +1,6 @@
 extends pushableObject
 
-const NUM_POINTS = 128
+const NUM_POINTS = 8
 const FRONT_THICKNESS = 2
 
 const mediumIndex = 1.0
@@ -12,7 +12,9 @@ const mediumIndex = 1.0
 
 var halfAngle = 1.0
 var lens_shape = PackedVector2Array()
-
+var lensRadius = 100.0
+var lensOrigin:Vector2 = Vector2.LEFT
+var minX = 1.0
 @onready var curveShape = $Stage/curveFaceArea/curveFaceShape
 @onready var flatShape = $Stage/flatFaceArea/flatFaceShape
 @onready var lensOutline = $Stage/LensOutline
@@ -21,44 +23,46 @@ var lens_shape = PackedVector2Array()
 
 func _ready():
 	isEnergizeable = false
-	var radius = focalLength * (lensIndex - mediumIndex)
-	set_geometry(radius,LENS_HEIGHT,Vector2.ZERO)
+	lensRadius = focalLength * (lensIndex - mediumIndex)-15.0
+	
+	set_geometry(lensRadius,LENS_HEIGHT,Vector2.ZERO)
 	frontFocusSprite.position = Vector2(focalLength,0.0)
 	rearFocusSprite.position = Vector2(-focalLength,0.0)
 	frontFocusSprite.self_modulate = focalSpriteColor
 	rearFocusSprite.self_modulate = focalSpriteColor
 	
 func set_geometry(newRadius:float, lens_height:float, center:Vector2):
-	var radius = newRadius
-	halfAngle = asin(lens_height / (2.0 * radius))
+	var rad = newRadius
+	halfAngle = asin(lens_height / (2.0 * rad))
 	curveShape.position.x = center[0]+FRONT_THICKNESS/2.0+1.0
 	curveShape.position.y = center[1]
-	var minX = radius - radius*cos(halfAngle)
+	lensOrigin = Vector2(-lensRadius+curveShape.position.x+minX/2.0,-curveShape.position.y)
+	minX = rad - rad*cos(halfAngle)
 	flatShape.shape.size = Vector2(FRONT_THICKNESS,LENS_HEIGHT)
 	flatShape.position = Vector2(-(minX+FRONT_THICKNESS)/2.0,0.0)
-	set_curve_polygon(minX, radius)
+	set_curve_polygon(minX, rad)
 
-func set_curve_polygon(minX : float, radius : float):
+func set_curve_polygon(minXVal : float, rad : float):
 	var poly = PackedVector2Array()
 
 	var spacing = 2*halfAngle / (NUM_POINTS-1)
 
 	for i in NUM_POINTS:
 		var theta = -halfAngle + spacing*i
-		poly.append(Vector2(radius*cos(theta)-radius+curveShape.position.x+minX/2.0,radius*sin(theta)+curveShape.position.y))
-		lens_shape.append(Vector2((radius)*cos(theta)-radius+2*curveShape.position.x+minX/2.0-FRONT_THICKNESS,(radius)*sin(theta)+2*curveShape.position.y))
+		poly.append(Vector2(rad*cos(theta)-rad+curveShape.position.x+minXVal/2.0,rad*sin(theta)+curveShape.position.y))
+		lens_shape.append(Vector2((rad)*cos(theta)-rad+2*curveShape.position.x+minXVal/2.0-FRONT_THICKNESS,(rad)*sin(theta)+2*curveShape.position.y))
 	
-	lens_shape.append(Vector2(-minX/2.0 - FRONT_THICKNESS,(radius)*sin(-halfAngle + spacing*(NUM_POINTS-1))+curveShape.position.y))
-	lens_shape.append(Vector2(-minX/2.0 - FRONT_THICKNESS,-radius*sin(-halfAngle + spacing*(NUM_POINTS-1))+curveShape.position.y))
+	lens_shape.append(Vector2(-minXVal/2.0 - FRONT_THICKNESS,(rad)*sin(-halfAngle + spacing*(NUM_POINTS-1))+curveShape.position.y))
+	lens_shape.append(Vector2(-minXVal/2.0 - FRONT_THICKNESS,-rad*sin(-halfAngle + spacing*(NUM_POINTS-1))+curveShape.position.y))
 		
 	for i in NUM_POINTS:
 		var theta = halfAngle - spacing*i
 		var insideRad
-		if radius > 0:
-			insideRad = radius-FRONT_THICKNESS
+		if rad > 0:
+			insideRad = rad-FRONT_THICKNESS
 		else:
-			insideRad = radius+FRONT_THICKNESS
-		poly.append(Vector2((insideRad)*cos(theta)-radius+curveShape.position.x+minX/2.0,(insideRad)*sin(theta)+curveShape.position.y))
+			insideRad = rad+FRONT_THICKNESS
+		poly.append(Vector2((insideRad)*cos(theta)-rad+curveShape.position.x+minX/2.0,(insideRad)*sin(theta)+curveShape.position.y))
 	
 	curveShape.polygon = poly
 	lensOutline.polygon = lens_shape
@@ -67,21 +71,32 @@ func get_face_polygon():
 	return curveShape.polygon
 	
 func _ray_hit(photonObj:Object, collPoint:Vector2, collNormal:Vector2, _collider:Object):
-	var normalIn = -collNormal;
-	var theta1 = normalIn.angle_to(photonObj.propDir);
-	var theta2
-	#Going from high index to low index
-	if photonObj.index_of_refraction > mediumIndex:
-		theta2 = asin(lensIndex/mediumIndex * sin(theta1))
-		photonObj.index_of_refraction = mediumIndex
-	#Going from low index to high index
+	if _collider.name == "curveFaceArea":
+		#Local collision point
+		var locPt = to_local(collPoint)
+		#Origin of the circle describing the lens rotated based on the lens rotation
+		var rotatedOrigin = lensOrigin.rotated(getRotation())
+		#Location along the sweep of the lens corrected for rotation
+		var correctedTheta = rotatedOrigin.angle_to_point(locPt)-getRotation()
+		#Perpendicular direction (facing inward)
+		var perpDir = -Vector2(cos(correctedTheta),sin(correctedTheta))
+		#Rotatation corrected
+		perpDir = perpDir.rotated(getRotation())
+		#var isChanged = false
+		#If the dot product is positive then the normal should be outward facing
+		if photonObj.propDir.dot(perpDir) > 0:
+		#	isChanged = true
+			perpDir = -perpDir#-Vector2(cos(-2*correctedTheta),sin(-2*correctedTheta))
+		photonObj.refractRay(perpDir, lensIndex, collPoint)
+		#### DEBUG STUFF ###########
+		#$Sprite2D.position = locPt#Vector2(25*cos(correctedTheta),25*sin(correctedTheta)) + locPt
+		#$Sprite2D2.position = 25*collNormal+locPt#Vector2(25*cos(correctedTheta),25*sin(correctedTheta)) + locPt
+		#$Sprite2D3.position = 25*perpDir + locPt
+		#print(photonObj.propDir.dot(perpDir),", ",photonObj.propDir.dot(collNormal))
+		#print(rad_to_deg(correctedTheta), ", ",rotatedOrigin,", ", perpDir,", ",collNormal)
+		#################################
 	else:
-		theta2 = asin(mediumIndex/lensIndex*sin(theta1))
-		photonObj.index_of_refraction = lensIndex
-		
-	var newDir = normalIn.rotated(theta2)
-	photonObj.propDir = newDir
-	photonObj.ray_add_point(photonObj.to_local(collPoint))
+		photonObj.refractRay(collNormal, lensIndex, collPoint)
 	
 func getFocusPositions():
 	var lensAngle = getRotation()
