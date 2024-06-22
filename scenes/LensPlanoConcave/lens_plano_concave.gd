6a9761ebc2c18f204ad4942ae3c29fe91498d14a
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-22 13:19:56 -0400

    Fixed concave lens

--- scenes/LensPlanoConcave/lens_plano_concave.gd
+++ scenes/LensPlanoConcave/lens_plano_concave.gd
@@ -19,6 +19,7 @@ var lensOrigin:Vector2 = Vector2.RIGHT
 @onready var flatShape = $Stage/BackFaceArea/flatFaceShape
 @onready var lensOutline = $Stage/LensOutline
 @onready var focusSprite = $Stage/focusSprite
+@onready var focusSprite2 = $Stage/focusSprite2
 @onready var botShape = $Stage/BottomFaceArea/BottomShape
 @onready var topShape = $Stage/TopFaceArea/TopShape
 
@@ -30,6 +31,7 @@ func _ready():
 	#$Line2D.set_point_position(1,Vector2(0,LENS_HEIGHT/2.0))
 
 	focusSprite.self_modulate = focalSpriteColor
+	focusSprite2.self_modulate=focalSpriteColor
 
 	if not isRotatable and initialAngle != 0:
 		var radAngle = deg_to_rad(initialAngle)
@@ -50,19 +52,21 @@ func set_geometry(newRadius:float, lens_height:float, center:Vector2):
 	#if maxY < lens_height:
 		#lens_height = 2*maxY
 
-	var lensThickness =40*FRONT_THICKNESS + maxX
+	var lensThickness =8*FRONT_THICKNESS + maxX
 	
 	#Distance from lens at theta=0 to principal plane
 	#I want this to be located at Zero
-	var apexToPlane = lensThickness/lensIndex
-	topShape.position = Vector2(-lensThickness/2.0+apexToPlane,-lens_height/2.0+FRONT_THICKNESS/2.0)
+	var apexToPlane = (lensThickness-maxX)/lensIndex
+	var backLoc = -(lensThickness-maxX)-apexToPlane
+	topShape.position = Vector2(backLoc+lensThickness/2.0,-lens_height/2.0+FRONT_THICKNESS/2.0)
 	topShape.shape.size = Vector2(lensThickness-2*FRONT_THICKNESS,FRONT_THICKNESS)
 	
-	botShape.position = Vector2(-lensThickness/2.0+apexToPlane,lens_height/2.0-FRONT_THICKNESS/2.0)
+	botShape.position = Vector2(backLoc+lensThickness/2.0,lens_height/2.0-FRONT_THICKNESS/2.0)
 	botShape.shape.size = Vector2(lensThickness-2*FRONT_THICKNESS,FRONT_THICKNESS)
 	
 	focusSprite.position = Vector2(focalLength,0.0)
-	$Sprite2D.position = Vector2(-apexToPlane,0)
+	focusSprite2.position = Vector2(-focalLength,0.0)
+	#$Sprite2D.position = Vector2(-apexToPlane,0)
 	#print(apexToPlane, ", ", minX)
 	#$Sprite2D.position = Vector2.ZERO
 	#$Sprite2D2.position = Vector2(apexToPlane,0)
@@ -71,7 +75,7 @@ func set_geometry(newRadius:float, lens_height:float, center:Vector2):
 	#print(lensRadius, lensOrigin)
 	#print("MaxX: ", maxX, ", ApexToPlane: ", apexToPlane, ", Thickness: ", lensThickness, ", Origin: ", lensOrigin)
 	flatShape.shape.size = Vector2(FRONT_THICKNESS,lens_height)
-	flatShape.position = Vector2(-(lensThickness-maxX)-apexToPlane+FRONT_THICKNESS/2.0,0.0)
+	flatShape.position = Vector2(backLoc+FRONT_THICKNESS/2.0,0.0)
 	set_curve_polygon(apexToPlane, newRadius, lensThickness)
 	#print($Stage/flatFaceArea/flatFaceShape.shape.size)
 
@@ -124,18 +128,20 @@ func _ray_hit(photonObj:Object, collPoint:Vector2, collNormal:Vector2, _collider
 		#	isChanged = true
 			perpDir = -perpDir#-Vector2(cos(-2*correctedTheta),sin(-2*correctedTheta))
 		photonObj.refractRay(perpDir, lensIndex, collPoint)
+		#print("Curve: ", photonObj.index_of_refraction)
 		#print(photonObj.index_of_refraction)
 		#print(collNormal, perpDir)
 		#### DEBUG STUFF ###########
 		#$Sprite2D.position = Vector2(-4*FRONT_THICKNESS+distToPrinPlane,-rad*sin(halfAngle))
 		#$Sprite2D.position = locPt#Vector2(25*cos(correctedTheta),25*sin(correctedTheta)) + locPt
-		$Sprite2D2.position = Vector2.ZERO#Vector2(25*cos(correctedTheta),25*sin(correctedTheta)) + locPt
-		#$Sprite2D.position = 25*perpDir + locPt
+		$Sprite2D2.position =25*collNormal + locPt#Vector2(25*cos(correctedTheta),25*sin(correctedTheta)) + locPt
+		$Sprite2D.position = 25*perpDir + locPt
 		#print(photonObj.propDir.dot(perpDir),", ",photonObj.propDir.dot(collNormal))
 		#print(rad_to_deg(correctedTheta), ", ",rotatedOrigin,", ", perpDir,", ",collNormal)
 		#################################
 	else:
 		photonObj.refractRay(collNormal, lensIndex, collPoint)
+		#print("Flat: ",photonObj.index_of_refraction)
 	
 func getFocusPositions():
 	var lensAngle = getRotation()
@@ -151,11 +157,19 @@ func update_texture():
 		tween.set_ease(Tween.EASE_IN)
 		tween.set_trans(Tween.TRANS_LINEAR)
 		tween.tween_property(focusSprite,"scale",Vector2(2,2),0.5)
+		var tween2 = get_tree().create_tween()
+		tween2.set_ease(Tween.EASE_IN)
+		tween2.set_trans(Tween.TRANS_LINEAR)
+		tween2.tween_property(focusSprite2,"scale",Vector2(2,2),0.5)
 	
 	else:
 		var tween = get_tree().create_tween()
 		tween.set_ease(Tween.EASE_IN)
 		tween.set_trans(Tween.TRANS_LINEAR)
 		tween.tween_property(focusSprite,"scale",Vector2(1,1),1)
+		var tween2 = get_tree().create_tween()
+		tween2.set_ease(Tween.EASE_IN)
+		tween2.set_trans(Tween.TRANS_LINEAR)
+		tween2.tween_property(focusSprite2,"scale",Vector2(1,1),1)
 		
 	
