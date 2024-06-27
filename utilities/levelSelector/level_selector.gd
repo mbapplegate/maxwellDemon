35e36ec6a4bc6cd886fb3fb8a357bbead77cf155
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-27 14:26:32 -0400

    Added labels and parsing of level select buttons

--- utilities/levelSelector/level_selector.gd
+++ utilities/levelSelector/level_selector.gd
@@ -1,31 +1,58 @@
 extends Node2D
 
 @onready var label = $Label
-@onready var levelButton = preload("res://utilities/levelSelectButton/level_select_button.tscn")
+
 signal nextScene(sceneAlias)
 
 const TEXT_COLOR = Color(0.2,0.2,0.2,1.0)
 const TEXT_TRANS = Color(0.2,0.2,0.2,0.0)
+const MAX_BUTTONS_PER_ROW : int = 10
 
 var orgChart = {}
+var aliasChart = {}
 
 func _ready():
 	for key in LevelInfo.GameFlow:
 		var thisSection = LevelInfo.LevelDictionary[key]["Section"]
 		if not orgChart.has(thisSection):
 			orgChart[thisSection] = 1
+			aliasChart[thisSection] = []
+			aliasChart[thisSection].append(key)
+		
 		else:
 			orgChart[thisSection] += 1
+			aliasChart[thisSection].append(key)
 			#numLevelsPerSection[sections.bsearch(LevelInfo.LevelDictionary[key]["Section"])] += 1
-	print(orgChart)
+		
 	label.self_modulate = TEXT_COLOR
-	$HBoxContainer/Level01.grab_focus()
-	var firstAlias = $HBoxContainer/Level01.levelAlias
-	label.text =  LevelInfo.LevelDictionary[firstAlias].Title
-	for child in $HBoxContainer.get_children():
-		if child is LevelSelectButton:
-			child.connect("pressed",buttonPressed.bind(child.levelAlias))
-			child.connect("focus_entered",buttonFocusEntered.bind(child.levelAlias))
+	var somethingHasFocus = false
+	for key in LevelInfo.SectionOrdering:
+		if orgChart.has(key):
+			var thisLabel = Label.new()
+			thisLabel.set("theme_override_font_sizes/font_size", 32)
+			thisLabel.text = key
+			$VBoxContainer.add_child(thisLabel)
+			var levelBox = HBoxContainer.new()
+			$VBoxContainer.add_child(levelBox)
+			var currentButtonsInRow = 0
+			for levelNum in range(orgChart[key]):
+				var thisButton = LevelSelectButton.new()
+				thisButton.text = "%02d" % (levelNum+1)
+				thisButton.levelAlias = aliasChart[key][levelNum]
+				levelBox.add_child(thisButton)
+				if not somethingHasFocus:
+						thisButton.grab_focus()
+						somethingHasFocus = true
+						label.text =  LevelInfo.LevelDictionary[thisButton.levelAlias].Title
+				thisButton.connect("pressed",buttonPressed.bind(thisButton.levelAlias))
+				thisButton.connect("focus_entered",buttonFocusEntered.bind(thisButton.levelAlias))
+				
+				currentButtonsInRow += 1
+				if currentButtonsInRow > MAX_BUTTONS_PER_ROW:
+					currentButtonsInRow = 0
+					levelBox = HBoxContainer.new()
+					$VBoxContainer.add_child(levelBox)
+	
 
 func buttonFocusEntered(levelAlias):
 	if label.self_modulate.a > 0:
