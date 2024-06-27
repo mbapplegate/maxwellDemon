377760dc2b89d50c9a10620530ef980614ae8378
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-27 15:17:25 -0400

    Changes to the level select screen so the buttons are auto-populated and auto-formatted based on the GameFlow variable in the level_info node. Still needs to be prettied up, but works as is.
    Squashed commit of the following:
    
    commit bcc47d5fcd11d0df9bf0056bb3bf0de95e05a499
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Thu Jun 27 15:15:12 2024 -0400
    
        Changed minimum size of buttons. Some formatting
    
    commit 35e36ec6a4bc6cd886fb3fb8a357bbead77cf155
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Thu Jun 27 14:26:32 2024 -0400
    
        Added labels and parsing of level select buttons
    
    commit 09dbb1675df156f5b8178f42172a9c2452d24278
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Wed Jun 26 20:49:49 2024 -0400
    
        Setup

--- utilities/levelInfo/level_info.gd
+++ utilities/levelInfo/level_info.gd
@@ -6,9 +6,9 @@ const WIRE_ON_COLOR = Color(240/255.0,240/255.0,60/255.0)
 signal imagingModeChanged(val)
 
 var LevelDictionary = {
-	"MainMenu"    : {"Path" : "res://utilities/mainMenu/main_menu.tscn",           "Title" : "Main Menu",      "Section" : "Menu"} ,
-	"LevelSelect" : {"Path" : "res://utilities/levelSelector/level_selector.tscn", "Title" : "Select Level",   "Section" : "Menu"},
-	"Manager"     : {"Path" : "res://utilities/gameManager/game_manager.tscn",     "Title" : "Game Manager",   "Section" : "Menu"},
+	"MainMenu"    : {"Path" : "res://utilities/mainMenu/main_menu.tscn",           "Title" : "Main Menu",      "Section" : "Menus"} ,
+	"LevelSelect" : {"Path" : "res://utilities/levelSelector/level_selector.tscn", "Title" : "Select Level",   "Section" : "Menus"},
+	"Manager"     : {"Path" : "res://utilities/gameManager/game_manager.tscn",     "Title" : "Game Manager",   "Section" : "Menus"},
 	"TestWorld"   : {"Path" : "res://worlds/test_world.tscn",                      "Title" : "Debug",          "Section" : "Debug"},
 	"Level001"    : {"Path" : "res://worlds/Level001/level_001.tscn",              "Title" : "Energize!",      "Section" : "Tutorial"},
 	"EnergizeLesson" : {"Path" : "res://lessons/energize/energize_lesson.tscn",    "Title" : "Energize Lesson", "Section" : "Lessons"},
@@ -21,23 +21,25 @@ var LevelDictionary = {
 	"Level005"    : {"Path" : "res://worlds/Level005/level_005.tscn",              "Title" : "Multiple Goals", "Section" : "Tutorial"},
 	"MeterLesson" : {"Path" : "res://lessons/meter/meter_lesson.tscn",             "Title" : "Meter Lesson",   "Section" : "Lessons"},
 	"DigitalMeterLesson" : {"Path" : "res://lessons/digitalMeter/digital_meter_lesson.tscn", "Title" : "Digital Meter Lesson", "Section" : "Lessons"},
-	"Level006"    : {"Path" : "res://worlds/Level006/level_006.tscn",              "Title" : "Mirrors",        "Section" : "Tutorial"},
+	"Level006"    : {"Path" : "res://worlds/Level006/level_006.tscn",              "Title" : "Mirrors",        "Section" : "Lenses and Mirrors"},
 	"MirrorLesson": {"Path" : "res://lessons/mirror/mirror_lesson.tscn",           "Title" : "Mirror Lesson",  "Section" : "Lessons"},
-	"Level007"    : {"Path" : "res://worlds/Level007/level_007.tscn",              "Title" : "Focus",          "Section" : "Tutorial"},
+	"Level007"    : {"Path" : "res://worlds/Level007/level_007.tscn",              "Title" : "Focus",          "Section" : "Lenses and Mirrors"},
 	"ConvexLensLesson"  : {"Path" : "res://lessons/convexLens/convex_lens_lesson.tscn", "Title" : "Convex Lens Lesson", "Section" : "Lessons"},
-	"Level007b"   : {"Path" : "res://worlds/Level007b_negLens/level_007b_negative_lens.tscn", "Title" : "Diverging Lens", "Section" : "LensesAndMirrors"},
+	"Level007b"   : {"Path" : "res://worlds/Level007b_negLens/level_007b_negative_lens.tscn", "Title" : "Diverging Lens", "Section" : "Lenses and Mirrors"},
 	"ConcaveLensLesson" : {"Path" : "res://lessons/concaveLens/concave_lens_lesson.tscn", "Title" : "Concave Lens Lesson", "Section" : "Lessons"},
-	"Level008"    : {"Path" : "res://worlds/Level008/level_008.tscn",              "Title" : "Collimation",    "Section" : "LensesAndMirrors"},
+	"Level008"    : {"Path" : "res://worlds/Level008/level_008.tscn",              "Title" : "Collimation",    "Section" : "Lenses and Mirrors"},
 	"CollimationLesson" : {"Path" : "res://lessons/collimation/collimation_lesson.tscn", "Title" : "Collimation Lesson", "Section" : "Lessons"},
-	"Level009"    : {"Path" : "res://worlds/Level009/level_009.tscn",              "Title" : "Expand",         "Section" : "LensesAndMirrors"},
+	"Level009"    : {"Path" : "res://worlds/Level009/level_009.tscn",              "Title" : "Expand",         "Section" : "Lenses and Mirrors"},
 	"BeamExpanderLesson" : {"Path" : "res://lessons/beamExpanderKep/beam_expander_lesson.tscn", "Title" : "Beam Expander Lesson", "Section" : "Lessons"},
-	"Level009b"   : {"Path" : "res://worlds/Level009b_negBeamExp/level_009b.tscn", "Title" : "Expand2",         "Section" : "LensesAndMirrors"},
+	"Level009b"   : {"Path" : "res://worlds/Level009b_negBeamExp/level_009b.tscn", "Title" : "Expand Again",         "Section" : "Lenses and Mirrors"},
 	"BeamExpanderLesson2" : {"Path" : "res://lessons/beamExpanderGal/beam_expander_gal_lesson.tscn", "Title" : "Galilan Beam Expander", "Section" : "Lessons"},
 	"Level010"    : {"Path" : "res://worlds/Level010/level_010.tscn",              "Title" : "Image",          "Section" : "Debug"},
-	"Level011"    : {"Path" : "res://worlds/Level011/level_011.tscn",              "Title" : "Parabolic Mirror","Section" : "LensesAndMirrors"},
+	"Level011"    : {"Path" : "res://worlds/Level011/level_011.tscn",              "Title" : "Parabolic Mirror","Section" : "Lenses and Mirrors"},
 	"ParabolicMirrorLesson" : {"Path" : "res://lessons/parabolicMirror/parabolic_mirror_lesson.tscn", "Title" : "Parabolic Mirror Lesson","Section" : "Lessons"},
 	"Level012Filters" : {"Path" : "res://worlds/Level012_Filters/level_012_filters.tscn", "Title" : "Filters", "Section" : "Colors"}
 }
+
+var SectionOrdering = ["Menus", "Tutorial", "Lenses and Mirrors", "Colors", "Lessons", "Debug"]
 
 var GameFlow = {
 	"Level001"              : "Level002",
