09dbb1675df156f5b8178f42172a9c2452d24278
Matt Applegate <matthewbapplegate@gmail.com>
2024-06-26 20:49:49 -0400

    Setup

--- utilities/levelSelector/level_selector.gd
+++ utilities/levelSelector/level_selector.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
 @onready var label = $Label
+@onready var levelButton = preload("res://utilities/levelSelectButton/level_select_button.tscn")
 signal nextScene(sceneAlias)
 
 const TEXT_COLOR = Color(0.2,0.2,0.2,1.0)
 const TEXT_TRANS = Color(0.2,0.2,0.2,0.0)
 
+var orgChart = {}
+
 func _ready():
+	for key in LevelInfo.GameFlow:
+		var thisSection = LevelInfo.LevelDictionary[key]["Section"]
+		if not orgChart.has(thisSection):
+			orgChart[thisSection] = 1
+		else:
+			orgChart[thisSection] += 1
+			#numLevelsPerSection[sections.bsearch(LevelInfo.LevelDictionary[key]["Section"])] += 1
+	print(orgChart)
 	label.self_modulate = TEXT_COLOR
 	$HBoxContainer/Level01.grab_focus()
 	var firstAlias = $HBoxContainer/Level01.levelAlias
