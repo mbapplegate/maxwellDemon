5f95aec3b0505fd55ab49bf1d9317aa637c68352
Matt Applegate <matthewbapplegate@gmail.com>
2024-07-05 18:43:24 -0400

    Added ability to shift objects +/- 2 pixels and rotate +/- 1 degree by holding Control. Pressing "C" snaps the object and player to the nearest grid position. Works, but might fail when trying adversarial actions. The whole Player script should be refactored
    Squashed commit of the following:
    
    commit 838fbc30e7dbf0ff4524616506c5304447d9ebf3
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Fri Jul 5 18:41:36 2024 -0400
    
        Fixed problem with pushing/pulling simultaneously. Added 'snap' command (c) to return item amd player to tile grid. Seems to work
    
    commit ab01a0bf5874c924bd0907bb8a4a41d47f21db19
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Fri Jul 5 12:39:30 2024 -0400
    
        Trying to diagnose bug. Turns out you always push then pull
    
    commit 27557a8fa288d44df430b316340162b4176ba042
    Author: Matt Applegate <matthewbapplegate@gmail.com>
    Date:   Thu Jul 4 16:43:39 2024 -0400
    
        Added nudge input for small motions/rotations. Still a bug when nudging objects against a wall

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -12,6 +12,7 @@ const SPRITE_SIZE = Vector2(64,64)
 const ACTIVATION_SIZE = 30
 const SEC_TO_IDLE = 2
 const TILE_SIZE = 32
+const NUDGE_DISTANCE = 2	
 
 var idle = false
 var idleCount = 0
@@ -21,8 +22,8 @@ var itemActive = null
 var tile_map
 var isMoving = false
 
-signal rotateCWSignal
-signal rotateCCWSignal
+signal rotateCWSignal(numDegrees)
+signal rotateCCWSignal(numDegrees)
 signal energizeSignal
 signal levelComplete
 signal objectActiveChanged(object, isActive)
@@ -52,32 +53,48 @@ func snap_position():
 	self.position = self.position.snapped(Vector2.ONE * TILE_SIZE)
 
 func _unhandled_input(event):
+	var numDegrees = deg_to_rad(15)
+	if Input.is_action_pressed("nudge"):
+		numDegrees = deg_to_rad(1)
 	if event.is_action_pressed("rotateCW"):
 		actionTaken= true
-		rotateCWSignal.emit()
+		rotateCWSignal.emit(numDegrees)
 	elif event.is_action_pressed("rotateCCW"):
 		actionTaken= true
-		rotateCCWSignal.emit()
+		rotateCCWSignal.emit(numDegrees)
 	elif event.is_action_pressed("toggleEnergize"):
 		energizeSignal.emit()
 		actionTaken= true
+	elif event.is_action_pressed("snap") and itemActive is pushableObject:
+		var snapped = itemActive.snapToGrid()
+		if snapped:
+			snap_position()
 	else:
 		pass
 				
 func move_grid(direction:String):
 	
-	var newPos = inputs[direction].normalized()*(TILE_SIZE) + self.global_position
-	if not isMoving and not Input.is_action_pressed("pull"):
+	var distToTravel = TILE_SIZE
+	if Input.is_action_pressed("nudge"):
+		distToTravel = NUDGE_DISTANCE
+	var newPos = inputs[direction].normalized()*(distToTravel) + self.global_position
+	if not isMoving and not Input.is_action_pressed("pull") and not Input.is_action_pressed("nudge"):
 		_set_activation_region(direction)
-		
-	if validate_movement(newPos, inputs[direction]) and not isMoving:
+	#print(validate_movement(newPos, inputs[direction]))	
+	if validate_movement(newPos, inputs[direction]):
 		var tween = get_tree().create_tween()
 		tween.set_ease(Tween.EASE_IN)
 		tween.set_trans(Tween.TRANS_CUBIC)
 		#print(itemActive)
-		if itemActive and Input.is_action_pressed("pull"):
-			itemActive.pull(inputs[direction],_player_body)
-		tween.tween_property(self,"global_position",newPos,0.2)
+		#if itemActive and Input.is_action_pressed("pull"):
+			#itemActive.pull(inputs[direction],_player_body)
+		#if itemActive and Input.is_action_pressed("nudge"):
+			#itemActive.nudgePull(inputs[direction],_player_body)
+			
+		if Input.is_action_pressed("nudge"):
+			tween.tween_property(self,"global_position",newPos,0.2)
+		else:
+			tween.tween_property(self,"global_position",newPos,0.2)
 		update_animation(inputs[direction])
 		isMoving = true
 		await tween.finished
@@ -91,6 +108,8 @@ func move_grid(direction:String):
 		
 	
 func validate_movement(testLoc:Vector2, direction:Vector2):
+	if isMoving:
+		return false
 	var dirCast1 = Vector2.ZERO
 	var dirCast2 = Vector2.ZERO
 	if direction == Vector2.RIGHT:
@@ -105,30 +124,40 @@ func validate_movement(testLoc:Vector2, direction:Vector2):
 	elif direction == Vector2.LEFT:
 		dirCast1 = (Vector2.LEFT + Vector2.UP).normalized()
 		dirCast2 = (Vector2.LEFT + Vector2.DOWN).normalized()
-		
+	
+	#if Input.is_action_pressed("nudge"):
+	#	distToLook = NUDGE_DISTANCE	
 	_cast.position = SPRITE_SIZE/2.0 + dirCast1*TILE_SIZE
 	_cast.target_position =to_local(testLoc)
 	_cast2.position = SPRITE_SIZE/2.0 + dirCast2*TILE_SIZE
 	_cast2.target_position = to_local(testLoc)
+	$Sprite2D.position = to_global(_cast.target_position)
 	#_cast.position = self.position
 	_cast.force_raycast_update()
 	_cast2.force_raycast_update()
 	if (not _cast.is_colliding()) and (not _cast2.is_colliding()):
-		return true
+		if itemActive and Input.is_action_pressed("pull"):
+			itemActive.pull(direction,_player_body)
+		if itemActive and Input.is_action_pressed("nudge"):
+			itemActive.nudgePull(direction,_player_body)
+		if itemActive:
+			return itemActive.isSliding
+		else:
+			return true
 	else:
 		var bothHittingSame = true
 		if _cast.is_colliding() and _cast2.is_colliding():
 			if _cast.get_collider() != _cast2.get_collider():
 				bothHittingSame = false
-		
+		#print("hitting same: ", bothHittingSame)
 		if _cast.is_colliding() and bothHittingSame:
-			if _cast.get_collider() is pushableObject and itemActive and _cast.get_collider().isPushable and Input.is_action_pressed("pull"):
+			if _cast.get_collider() is pushableObject and itemActive and _cast.get_collider().isPushable and (Input.is_action_pressed("nudge") or Input.is_action_pressed("pull")):
 				_cast.get_collider().push(_cast.target_position)
 				return _cast.get_collider().isSliding
 			else:
 				return false
 		elif _cast2.is_colliding() and bothHittingSame:
-			if _cast2.get_collider() is pushableObject and itemActive and _cast2.get_collider().isPushable and Input.is_action_pressed("pull"):
+			if _cast2.get_collider() is pushableObject and itemActive and _cast2.get_collider().isPushable and  (Input.is_action_pressed("nudge") or Input.is_action_pressed("pull")):
 				_cast2.get_collider().push(_cast2.target_position)
 				return _cast2.get_collider().isSliding
 			else:
@@ -157,7 +186,7 @@ func _physics_process(delta):
 			update_animation(inputs[dir])
 			move_grid(dir)
 			
-		if Input.is_action_pressed("pull") and not isMoving:
+		if (Input.is_action_pressed("pull") or Input.is_action_pressed("nudge")) and not isMoving:
 			actionTaken = true
 			
 	if not actionTaken:
@@ -184,7 +213,7 @@ func _physics_process(delta):
 	
 func update_animation(motion:Vector2):
 	var animation = _animation_player.animation
-	if not Input.is_action_pressed("pull"):
+	if not Input.is_action_pressed("pull") and not Input.is_action_pressed("nudge"):
 		if motion.x > 0:
 			_set_activation_region("right")
 			animation = "WalkRight"
